b6068949a470727bb540cad6a2cff76121909849
Thomas Gredig <thomas.gredig@csulb.edu>
2019-10-21 19:27:47 -0700

    change list to data.frame for sweep data

--- R/read.NID_sweepFile.R
+++ R/read.NID_sweepFile.R
@@ -1,7 +1,7 @@
 #' loads frequency sweep data from NID file
 #'
 #' @param filename filename including path
-#' @return list with data frames that have freq vs. ampl.
+#' @return data.frame with data frames that have freq vs. ampl.
 #' @examples
 #' filename = dir(pattern='nid$', recursive=TRUE)[1]
 #' d = read.NID_sweepFile(filename)
@@ -26,12 +26,12 @@ read.NID_sweepFile <- function(filename) {
   bin.header <- readBin(con, integer(),  n = header.length, size=1, endian = "little")
   bin.ID = readBin(con, integer(),  n = 2, size=1, endian = "little")
   #r = list(header = bin.header, ID = bin.ID)
-  r = list()
+  r = data.frame()
 
   if (sum(bin.ID) == sum(c(35,33))) {
     if(freq.len>0) {
         bin.data <- readBin(con, integer(),  n = freq.len, size=2, endian = "little")
-        r[[i]] = data.frame(x = seq(from=freq.min, to=freq.min + freq.range, length.out=freq.len),
+        r = data.frame(x = seq(from=freq.min, to=freq.min + freq.range, length.out=freq.len),
                             y = bin.data/A.len*A.range+A.min)
     }
   }

cccb6a50693642d2fd378cc62c0301ddd5a604ed
Thomas Gredig <thomas.gredig@csulb.edu>
2021-04-26 21:18:48 -0700

    include reading different channels

--- R/read.IBW_file.R
+++ R/read.IBW_file.R
@@ -1,26 +1,29 @@
 #' loads images
 #'
 #' @param filename filename including path
-#' @return list with header, file ID, and images
+#' @param no number of the channel
+#' @return image with attributes
 #' @examples
 #' filename = dir(pattern='ibw$', recursive=TRUE)[1]
 #' d = read.IBW_file(filename)
 #' @export
-read.IBW_file <- function(filename) {
+read.IBW_file <- function(filename, no=1) {
+  no = 2
   d = read.ibw(filename)
-  q1 = read.ibw(filename, HeaderOnly = TRUE)
-  q2 = attr(q1, "WaveHeader")
-  imagePixels = q2$nDim[1]
-  sfA = q2['sfA']
-  imageWidths.um = sfA$sfA*1e6*(imagePixels-1)
-
+  q2 = attr(d, "WaveHeader")
   x = 1:dim(d)[1]
   y = 1:dim(d)[2]
+  z = 1:(length(x)*length(y)) + (no-1)*(length(x)*length(y))
   dr = data.frame(
-    x = (rep(x, length(y))-1)*sfA$sfA[1]*1e6,
-    y = (rep(y, each=length(x))-1)*sfA$sfA[1]*1e6,
-    z.nm = d[1:(length(x)*length(y))]*1e9
+    x = rep(x, length(y)),
+    y = rep(y, each=length(x)),
+    z.nm = d[z]*1e9
   )
   dr$z.nm=dr$z.nm-min(dr$z.nm)
+  attr(dr,"imageDim") <- q2$nDim[1:2]
+  attr(dr,"noChannels") <- q2$nDim[3]
+  sfA = q2['sfA']$sfA
+  attr(dr,"convFactor") <- sfA[no]
+  attr(dr,"Units") <- q2$dimUnits[no]
   dr
 }
